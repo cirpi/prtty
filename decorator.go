9aa3c8f77b0a577f8e7eae9019a5e4fdee513795
Sirpi Yugendar <sirpiyugendarr@gmail.com>
2025-03-22 19:54:29 +0530

    i'm stupid

--- decorator.go
+++ decorator.go
@@ -26,6 +26,5 @@ func (d *decorator) String(str string) string {
 	for _, style := range d.styles {
 		styleString.WriteString(string(style))
 	}
-	styleString.WriteString(string(Reset))
-	return fmt.Sprintf("%s%s", styleString.String(), str)
+	return fmt.Sprintf("%s%s%s", styleString.String(), str, Reset)
 }
